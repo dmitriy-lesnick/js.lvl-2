6cf7e3dc6fdcfa5efdef5386adf35d7b1504b24e
lesnick <dima.mk92@mail.ru>
2023-02-13 11:10:38 +0300

    lesson1; wip

---
+++ script.js
@@ -0,0 +1,6 @@
+const fs = require('fs')
+
+const text = fs.readFileSync('scrip.js', 'utf8')
+
+console.log(text)
+
---
+++ server.js
@@ -0,0 +1,17 @@
+const http = require('http')
+
+const fs = require('fs')
+
+const server = http.createServer((req, res) => {
+
+    const body = req.url === '/style.css'
+        ? fs.readFileSync('./public/style.css')
+        : fs.readFileSync('./public/index.html')
+
+    res.end(body)
+
+})
+
+server.listen(3000)
+
+console.log('Server started!')
