465d04fbe2ff6aa10846008910bbc0b8eddb865f
lesnick <dima.mk92@mail.ru>
2023-02-14 11:51:46 +0300

    less2

---
+++ public/script.js
@@ -0,0 +1,77 @@
+class Button {
+    _text = ''
+    constructor(text) {
+        this._text = text
+        /* this.render() */
+    }
+
+    get text() {
+        return this._text
+    }
+
+    set text(value) {
+        this._text = value
+    }
+
+    onBtnClick() {
+        console.log('clicked')
+    }
+
+    getTemplate() {
+        const btn = document.createElement('button')
+        btn.classList.add('btn')
+        return btn
+    }
+
+    render() {
+        const placeToRender = document.querySelector('.buttons')
+        if (placeToRender) {
+            const btn = this.getTemplate()
+            btn.innerText = this.text
+            placeToRender.appendChild(btn)
+            btn.addEventListener('click', () => {
+                this.onBtnClick()
+            })
+        }
+    }
+}
+
+class AlertButton extends Button {
+    constructor(text) {
+        super(text)
+    }
+
+    onBtnClick() {
+        alert('clicked')
+    }
+}
+
+class RoundButton extends Button {
+    constructor(text) {
+        super(text)
+    }
+
+    getTemplate() {
+        const btn = document.createElement('button')
+        btn.classList.add('round')
+        return btn
+    }
+
+    onBtnClick() {
+        console.log('round clicked')
+    }
+
+}
+
+new Button('Click me')
+
+const alertButton = new AlertButton('AlERT click')
+
+const roundButton = new RoundButton('round btn')
+
+const btns = [alertButton, roundButton]
+
+btns.forEach(btn => {
+    btn.render()
+})
+
