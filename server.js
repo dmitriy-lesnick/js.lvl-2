390502d8aafa134160eb8277cf85c8d14e561fb5
lesnick <dima.mk92@mail.ru>
2023-02-14 10:08:22 +0300

    styles

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const fs = require('fs')
 
 const server = http.createServer((req, res) => {
     console.log(req.url)
-    const body = req.url === '/style.css'
-        ? fs.readFileSync('./public/style.css')
+    const body = req.url === '/styles.css'
+        ? fs.readFileSync('./public/styles.css')
         : fs.readFileSync('./public/index.html')
 
     res.end(body)
