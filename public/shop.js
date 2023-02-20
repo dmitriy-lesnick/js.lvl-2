941bce5067b5d563c31e31e0c5c89c25b0dd9976
lesnick <dima.mk92@mail.ru>
2023-02-20 09:03:18 +0300

    k

--- public/shop.js
+++ public/shop.js
@@ -46,31 +46,32 @@ class List extends AbstractList {
 
 }
 
-const placeToRender = document.querySelector('header')
 
-if (placeToRender) {
 
-    const block = document.createElement('div')
-
-    block.classList.add('cart')
-
-    const ButtonInstance = new Button('Корзина')
-
-    block.appendChild(ButtonInstance.getTemplate())
-
-    placeToRender.appendChild(block)
-}
-
-/* class Cart extends AbstractList {
+class Cart extends AbstractList {
 
     constructor() {
         super()
     }
 
     init() {
-        
+        const placeToRender = document.querySelector('header')
+
+        if (placeToRender) {
+
+            const block = document.createElement('div')
+
+            block.classList.add('cart')
+
+            const ButtonInstance = new Button('Корзина')
+
+            block.appendChild(ButtonInstance.getTemplate())
+
+            placeToRender.appendChild(block)
+        }
+
     }
-} */
+}
 
 
 class GoodsItem {
